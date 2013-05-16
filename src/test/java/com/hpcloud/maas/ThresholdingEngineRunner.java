87a2d99851e097071538c0f48f445e6c52c71c41
Jonathan Halterman <jhalterman@gmail.com>
2013-05-16 15:12:22 -0700

    Use pi test config by default

--- src/test/java/com/hpcloud/maas/ThresholdingEngineRunner.java
+++ src/test/java/com/hpcloud/maas/ThresholdingEngineRunner.java
@@ -2,6 +2,6 @@ package com.hpcloud.maas;
 
 public class ThresholdingEngineRunner {
   public static void main(String... args) throws Exception {
-    ThresholdingEngine.main("test-config.yml", "test-topo", "1");
+    ThresholdingEngine.main("config-pi-test.yml", "test-topo", "1");
   }
 }
