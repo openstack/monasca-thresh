74172dbf14b7fa561a8fec234049ce4d0d97a9c2
Jonathan Halterman <jhalterman@gmail.com>
2013-05-17 12:09:48 -0700

    Enabled db test by default

--- src/test/java/com/hpcloud/maas/infrastructure/persistence/MetricDefinitionDAOImplTest.java
+++ src/test/java/com/hpcloud/maas/infrastructure/persistence/MetricDefinitionDAOImplTest.java
@@ -21,7 +21,7 @@ import com.hpcloud.maas.domain.service.MetricDefinitionDAO;
  * 
  * @author Jonathan Halterman
  */
-@Test(groups = "database", enabled = false)
+@Test(groups = "database")
 public class MetricDefinitionDAOImplTest {
   private DBI db;
   private Handle handle;
@@ -41,27 +41,30 @@ public class MetricDefinitionDAOImplTest {
 
   @BeforeMethod
   protected void beforeMethod() {
-    handle.execute("delete from alarm where id = 123");
+    handle.execute("delete from sub_alarm where id in (111, 222, 333)");
+    handle.execute("delete from sub_alarm_dimension where sub_alarm_id in (111, 222, 333)");
 
-    handle.execute("insert into alarm (id, tenant_id, name, expression, state, created_at, updated_at) "
-        + "values ('123', 'bob', '90% CPU', 'avg(hpcs.compute:cpu:{flavor_id=777, image_id=888}) > 10', 'UNDETERMINED', NOW(), NOW())");
     handle.execute("insert into sub_alarm (id, alarm_id, function, namespace, metric_type, metric_subject, operator, threshold, period, periods, state, created_at, updated_at) "
         + "values ('111', '123', 'AVG', 'hpcs.compute', 'cpu', '1', 'GT', 10, 60, 1, 'OK', NOW(), NOW())");
+    handle.execute("insert into sub_alarm_dimension values ('111', 'instance_id', '777')");
+    handle.execute("insert into sub_alarm_dimension values ('111', 'image_id', '888')");
+
     handle.execute("insert into sub_alarm (id, alarm_id, function, namespace, metric_type, metric_subject, operator, threshold, period, periods, state, created_at, updated_at) "
         + "values ('222', '123', 'AVG', 'hpcs.compute', 'mem', null, 'GT', 10, 60, 1, 'OK', NOW(), NOW())");
-    handle.execute("insert into sub_alarm_dimension values ('111', 'flavor_id', '777')");
-    handle.execute("insert into sub_alarm_dimension values ('111', 'image_id', '888')");
     handle.execute("insert into sub_alarm_dimension values ('222', 'instance_id', '123')");
     handle.execute("insert into sub_alarm_dimension values ('222', 'az', '2')");
+
+    handle.execute("insert into sub_alarm (id, alarm_id, function, namespace, metric_type, metric_subject, operator, threshold, period, periods, state, created_at, updated_at) "
+        + "values ('333', '123', 'AVG', 'foo', 'bar', null, 'GT', 10, 60, 1, 'OK', NOW(), NOW())");
   }
 
   public void shouldFindForAlarms() {
-    List<MetricDefinition> expected = Arrays.asList(new MetricDefinition("hpcs.compute", "cpu", "1",
-        ImmutableMap.<String, String>builder()
-            .put("flavor_id", "777")
-            .put("image_id", "888")
-            .build()), new MetricDefinition("hpcs.compute", "mem", null,
-        ImmutableMap.<String, String>builder().put("instance_id", "123").put("az", "2").build()));
+    MetricDefinition metricDef1 = new MetricDefinition("hpcs.compute", "cpu", "1",
+        ImmutableMap.<String, String>builder().put("instance_id", "777").build());
+    MetricDefinition metricDef2 = new MetricDefinition("hpcs.compute", "mem", null,
+        ImmutableMap.<String, String>builder().put("instance_id", "123").build());
+    MetricDefinition metricDef3 = new MetricDefinition("foo", "bar", null, null);
+    List<MetricDefinition> expected = Arrays.asList(metricDef1, metricDef2, metricDef3);
 
     assertTrue(dao.findForAlarms().containsAll(expected));
   }
