0cb9a960162ebf1087038dd814cbc2e2eee02b10
Jonathan Halterman <jhalterman@gmail.com>
2013-05-30 20:27:55 -0700

    Removed logging from deserializer

--- src/main/java/com/hpcloud/maas/infrastructure/thresholding/deserializer/MaasMetricDeserializer.java
+++ src/main/java/com/hpcloud/maas/infrastructure/thresholding/deserializer/MaasMetricDeserializer.java
@@ -5,9 +5,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import backtype.storm.tuple.Fields;
 
 import com.hpcloud.maas.common.model.metric.FlatMetric;
@@ -25,7 +22,6 @@ import com.hpcloud.maas.infrastructure.storm.TupleDeserializer;
  * @author Jonathan Halterman
  */
 public class MaasMetricDeserializer implements TupleDeserializer, Serializable {
-  private static final Logger LOG = LoggerFactory.getLogger(MaasMetricDeserializer.class);
   private static final long serialVersionUID = 4021288586913323048L;
   private static final Fields FIELDS = new Fields("metricDefinition", "metric");
 
@@ -33,7 +29,6 @@ public class MaasMetricDeserializer implements TupleDeserializer, Serializable {
   public List<List<?>> deserialize(byte[] tuple) {
     FlatMetric flatMetric = FlatMetrics.fromJson(tuple);
     Metric metric = flatMetric.toMetric();
-    LOG.trace("Deserialized MaaS metric: {}", metric);
     return Collections.<List<?>>singletonList(Arrays.asList(metric.definition, metric));
   }
 
